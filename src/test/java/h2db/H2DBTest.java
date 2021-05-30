a5998044574632d9b83b4d360dd567b77cc2e92a
uraven0107 <turn.pso@gmail.com>
2021-05-30 14:15:09 +0900

    :pencil: add test for H2DB

---
+++ src/test/java/h2db/H2DBTest.java
@@ -0,0 +1,30 @@
+package h2db;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.h2.Driver;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class H2DBTest {
+
+    @Test
+    public void testRunningH2DB() throws Exception{
+        Driver.load();
+        Connection con = DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        Statement st = con.createStatement();
+        st.execute("create table test (id int primary key,name varchar)");
+        st.execute("insert into test values (1, 'hoge')");
+        ResultSet rs = st.executeQuery("select * from test");
+        rs.next();
+        assertEquals(1, rs.getInt("id"));
+        assertEquals("hoge", rs.getString("name"));
+        assertFalse(rs.next());
+        st.close();
+        con.close();
+    }
+}
